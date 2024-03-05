b9dad557e29e91a49885396b6b15ca9d14b6b3fc
bayan-alqasem <bayan96al-qasem@hotmail.com>
2024-03-06 00:15:19 +0300

    blackStoneProject

---
+++ src/TestCases/LoginTest.java
@@ -0,0 +1,70 @@
+package TestCases;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends Parameter {
+
+	@BeforeTest
+	public void setUp() {
+
+		driver.manage().window().maximize();
+	}
+
+	@Test(priority = 1)
+	public void logInTest() throws InterruptedException {
+
+		driver.get(URL);
+
+		WebElement emailAddress = driver.findElement(By.id("mat-input-0"));
+		emailAddress.sendKeys(email);
+
+		WebElement thePassword = driver.findElement(By.id("mat-input-1"));
+		thePassword.sendKeys(password);
+
+		WebElement sginInButton = driver.findElement(By.cssSelector(".block.primary"));
+		sginInButton.click();
+
+		Thread.sleep(50000);
+
+		String expectedSuccessfulLoginMessage = "Welcome To Dashboard";
+		String actualSuccessfulLoginMessage = driver
+				.findElement(By.xpath("/html/body/app-root/admin-layout/section/div[3]/dashboard/p")).getText();
+
+		assertEquals(actualSuccessfulLoginMessage, expectedSuccessfulLoginMessage);
+	}
+
+	@Test(priority = 2)
+	public void homePageTitle() {
+		String homePageTitle = driver.getTitle();
+		System.out.println(homePageTitle);
+
+	}
+
+	@Test(priority = 3)
+	public void logOutTest() throws InterruptedException {
+		WebElement img = driver.findElement(By.xpath(
+				"/html/body/app-root/admin-layout/section/div[1]/header/section/navbar/div/div[2]/nav-actions/div/ul/li[3]/div/div[2]/img"));
+		img.click();
+
+		WebElement ulContainer = driver.findElement(By.xpath(
+				"/html/body/app-root/admin-layout/section/div[1]/header/section/navbar/div/div[2]/nav-actions/div/ul/li[3]/div/div[2]/ul"));
+		Thread.sleep(2000);
+		ulContainer.findElements(By.tagName("li")).get(2).click();
+
+		boolean actual = driver.findElement(By.className("login")).getText().contains("Sign In");
+		assertEquals(actual, true);
+
+	}
+
+	@AfterTest
+	public void postTesting() throws InterruptedException {
+		Thread.sleep(1000);
+		driver.quit();
+	}
+}
---
+++ src/TestCases/Parameter.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Parameter {
+	WebDriver driver = new ChromeDriver();
+	String URL = "https://oyn-adminportal-qc-demo.salmonsky-1edff179.westeurope.azurecontainerapps.io/";
+	String email="store@admin.com";
+	String password="P@ssw0rd";
+	
+
+}
