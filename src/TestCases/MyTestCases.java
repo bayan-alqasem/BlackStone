76ca3fae70582e5f49e0501444a9df582655c9e1
bayan-alqasem <bayan96al-qasem@hotmail.com>
2024-03-06 23:09:12 +0300

    fix

--- src/TestCases/MyTestCases.java
+++ src/TestCases/MyTestCases.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class LoginTest extends Parameter {
+public class MyTestCases extends Parameter {
 
 	@BeforeTest
 	public void setUp() {
